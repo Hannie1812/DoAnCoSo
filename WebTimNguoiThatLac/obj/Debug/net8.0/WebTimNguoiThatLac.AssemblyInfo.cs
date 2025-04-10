5f4dfd150294713552d801444ef661179140f5c8
Hannie1812 <bichhang18122004@gmail.com>
2025-04-10 15:32:08 +0700

    Chỉnh màu 2 nút login GG&Fb

--- WebTimNguoiThatLac/obj/Debug/net8.0/WebTimNguoiThatLac.AssemblyInfo.cs
+++ WebTimNguoiThatLac/obj/Debug/net8.0/WebTimNguoiThatLac.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebTimNguoiThatLac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8dfde4b7de6f0fd636698317f512eda2fb86127")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b19e2d197cf8ae9bdbec135e610ced131ecc80d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebTimNguoiThatLac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebTimNguoiThatLac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
